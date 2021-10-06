3b6c9dd42344b4bd53e095e9c0b0691bfb12765b
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2021-10-06 11:44:48 +0200

    Getter and Setter add

--- src/test/java/User.java
+++ src/test/java/User.java
@@ -25,4 +25,44 @@ public class User {
             return false;
         }
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public void setAdult(boolean adult) {
+        isAdult = adult;
+    }
 }
