48e8185bc4bf7784ed1c45b818483e780541444e
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2021-10-07 11:22:39 +0200

    Static methods 1

--- src/test/java/MainApp.java
+++ src/test/java/MainApp.java
@@ -1,26 +1,16 @@
-import model.Bug;
+import Utils.StringUtils;
+
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-        int[] numbers = {2, 4, 1, 9, 5, 2, 4, 8, 5, 0};
+        String bartekTest = StringUtils.getFormattedText("KrzysiekWaliszewski");
+        System.out.println(bartekTest);
 
-        int max;
-        for (int i = 0; i < numbers.length; i++) {
-            max = numbers[i];
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[j] > max) {
-                    max = numbers[j];
-                    numbers[j] = numbers[i];
-                    numbers[i] = max;
-                }
-            }
-        }
-        for (int number : numbers
-        ) {
-            System.out.println(number);
-        }
+        String bartek = StringUtils.getFormattedText("Krzyś");
+        System.out.println(bartek);
+
 
     }
 }
