af18cd71ca7671c81a4034322ca4d1485901a2f3
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2021-10-07 09:12:15 +0200

    Tablice

--- src/test/java/MainApp.java
+++ src/test/java/MainApp.java
@@ -4,12 +4,23 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-        Bug bug = new Bug("Short", "ironking666@wp.pl", 3);
+        int[] numbers = {2, 4, 1, 9, 5, 2, 4, 8, 5, 0};
 
-        bug.showAllBugs();
-        bug.setBugDescription("Longlonglong");
-        bug.setEmail("ironkingironking@wp.pl");
-        bug.setBugPriority(-1);
-        bug.showAllBugs();
+        int max;
+        for (int i = 0; i < numbers.length; i++) {
+            max = numbers[i];
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] > max) {
+                    max = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = max;
+                }
+            }
+        }
+        for (int number : numbers
+        ) {
+            System.out.println(number);
+        }
+
     }
 }
